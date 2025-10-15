086ac0780728ced46158bc15aa120b812e9d0f79
qppd <quezon.province.pd@gmail.com>
2025-10-16 03:17:28 +0800

    servo checked

--- source/rpi/test/servo_tester.py
+++ source/rpi/test/servo_tester.py
@@ -7,25 +7,34 @@
 import time
 import pigpio
 
-PIN = 22  # BCM numbering
+SERVO_PIN = 22  # BCM numbering
 
 pi = pigpio.pi()
 if not pi.connected:
     print("Could not connect to pigpio daemon!")
     exit(1)
 
+# For 180° servo:
+# 0°   = 500us
+# 90°  = 1500us
+# 180° = 2500us
+
+def angle_to_pulse(angle):
+    # Map angle (0-180) to pulse width (500-2500us)
+    return int(500 + (angle / 180.0) * 2000)
+
 try:
     while True:
-        print("GPIO22 HIGH")
-        pi.write(PIN, 1)
-        time.sleep(5)
-        print("GPIO22 LOW")
-        pi.write(PIN, 0)
-        time.sleep(5)
+        print("Moving to 40°")
+        pi.set_servo_pulsewidth(SERVO_PIN, angle_to_pulse(40))
+        time.sleep(2)
+        print("Moving to 90°")
+        pi.set_servo_pulsewidth(SERVO_PIN, angle_to_pulse(90))
+        time.sleep(2)
 except KeyboardInterrupt:
     pass
 finally:
-    pi.write(PIN, 0)
+    pi.set_servo_pulsewidth(SERVO_PIN, 0)  # Stop servo
     pi.stop()
     
     print("Servo test finished.")
